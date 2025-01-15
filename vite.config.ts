a520a5d965f2f0dde9851bda095089dc732eeca9
Ethan Chan <bbxycx.18@163.com>
2025-01-15 20:12:20 +0800

    feat: 优化构建

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 // @ts-ignore
-export default ({ mode }) => {
+export default ({mode}) => {
   console.log('mode', loadEnv(mode, process.cwd()).VITE_BASE_URL); //127.0.0.1:9000/api
+  // const env = loadEnv(mode, process.cwd()) as ImportMetaEnv
 
   return defineConfig({
     plugins: [react()],
@@ -15,7 +16,28 @@ export default ({ mode }) => {
       },
     },
     build: {
-      target: "ESNext"
+      target: "ESNext",
+      chunkSizeWarningLimit: 2000, // 消除打包大小超过500kb警告
+      outDir: 'dist', // 指定打包路径，默认为项目根目录下的dist目录
+      // minify: 'terser', // Vite 2.6.x 以上需要配置 minify："terser"，terserOptions才能生效
+      // terserOptions: {
+      //   compress: {
+      //     keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
+      //     drop_console: true, // 生产环境去除 console
+      //     drop_debugger: true // 生产环境去除 debugger
+      //   },
+      //   format: {
+      //     comments: false // 删除注释
+      //   }
+      // },
+      // 静态资源打包到dist下的不同目录
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+        }
+      }
     },
     server: {
       host: '0.0.0.0',
